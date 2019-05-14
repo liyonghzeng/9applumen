a72de005749f5420079558eaf371a9c82af1a6ca
Your Name <liyonghzeng@2548722906@qq.com>
2019-05-14 11:03:31 +0000

    token

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -40,16 +40,11 @@ class LoginController extends BaseController
             'password'=>$base_i
         ];
         $res=DB::table('zcc')->insertGetId($where);
-        $token=md5(Str::random(15).'lyz'.time());
-        $ksy_token='login_token:uid'.$res;
-        Redis::set($ksy_token,$token);
-        Redis::expire($ksy_token,259200);
+
         if($res){
            $json=[
                'erron'=>0,
                'mag'=>'注册成功成功',
-               'token'=>$token,
-               'uid'=>$res,
            ];
            $dd=json_encode($json);
            echo $dd;
@@ -92,10 +87,15 @@ class LoginController extends BaseController
             $public_key=openssl_pkey_get_public("file://".storage_path("key/public.pem"));
              openssl_public_decrypt($new_data,$ii,$public_key);
              if($ii == $pwd){
+                 $token=md5(Str::random(15).'lyz'.time());
+                 $ksy_token='login_token:uid'.$res->u_id;
+                 Redis::set($ksy_token,$token);
+                 Redis::expire($ksy_token,259200);
                  $json=[
                      'erron'=>0,
                      'mag'=>'登录成功',
-
+                     'token'=>$token,
+                     'uid'=>$res->u_id
                  ];
                  $dd=json_encode($json);
                return $dd;
